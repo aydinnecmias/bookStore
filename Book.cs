21b13a2f230203425da0728a1d6abfa2e1d8ed0a
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-08-03 11:50:57 +0300

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookStore
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int GenreId { get; set; }
+        public int PageCount { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+    }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+
+    public class BookController : ControllerBase
+    {
+
+        private static List<Book> BookList = new List<Book>()
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1,
+                PageCount = 200,
+                PublishDate = new DateTime(2001,06,12) 
+
+            }
+
+        };
+
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;                   
+
+        }
+
+        [HttpGet("{id}")]
+
+        public Book GetById(int id)
+        {
+            var book = BookList.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+
+        }
+    }
+}
