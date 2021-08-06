a8d42f8ae8851674ce052eaa6755b58cc6d91626
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-08-06 18:11:43 +0300

    Get,Post,Put,Delete Operasyonları veritabanı olmaksızın gerçekleştirildi.

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -20,7 +20,7 @@ namespace bookStore.Controllers
                 Title = "Lean Startup",
                 GenreId = 1,
                 PageCount = 200,
-                PublishDate = new DateTime(2001,06,12) 
+                PublishDate = new DateTime(2001,06,12)
 
             }
 
@@ -30,7 +30,7 @@ namespace bookStore.Controllers
         public List<Book> GetBooks()
         {
             var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
-            return bookList;                   
+            return bookList;
 
         }
 
@@ -40,6 +40,50 @@ namespace bookStore.Controllers
         {
             var book = BookList.Where(book => book.Id == id).SingleOrDefault();
             return book;
+
+        }
+
+        [HttpPost]
+
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null)
+                return BadRequest();
+            BookList.Add(newBook);
+            return Ok();
+
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult UpdateBook(int id, [FromBody] Book UpdatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+
+            if (book is null)
+                return BadRequest();
+            book.GenreId = UpdatedBook.GenreId != default ? UpdatedBook.GenreId : book.GenreId;
+            book.PageCount = UpdatedBook.PageCount != default ? UpdatedBook.PageCount : book.PageCount;
+            book.PublishDate = UpdatedBook.PublishDate != default ? UpdatedBook.PublishDate : book.PublishDate;
+            book.Title = UpdatedBook.Title != default ? UpdatedBook.Title : book.Title;
+
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeletedBook(int id)
+        {
+            var book = BookList.SingleOrDefault(x=> x.Id == id);
+
+            if (book is null)
+                return BadRequest();
+
+            BookList.Remove(book);
+            return Ok();
+
 
         }
     }
