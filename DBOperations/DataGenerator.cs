d498c0237e83d034b366c898f5a7729e07cd8e96
Aydın Necmi AS <necmi_as1@hotmail.com>
2021-08-06 23:46:37 +0300

    EF Core ve InMemory Eklendi. DBOperations eklendi. Program.cs ve Startup.cs'den başlangıç tetiklemesi yapıldı.

---
+++ DBOperations/DataGenerator.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookStore.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+
+                }
+                context.Books.AddRange(
+
+                    new Book
+                    {
+                        Id = 1,
+                        Title = "Lean Startup",
+                        GenreId = 1,
+                        PageCount = 200,
+                        PublishDate = new DateTime(2001, 06, 12)
+
+                    },
+                    new Book
+                    {
+                        Id = 2,
+                        Title = "Ben Robot",
+                        GenreId = 1,
+                        PageCount = 500,
+                        PublishDate = new DateTime(2001, 06, 12)
+
+                    },
+                    new Book
+                    {
+                        Id = 3,
+                        Title = "Kararsızlar",
+                        GenreId = 1,
+                        PageCount = 2300,
+                        PublishDate = new DateTime(2001, 06, 12)
+
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using bookStore.DBOperations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,12 +28,15 @@ namespace bookStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "bookStore", Version = "v1" });
             });
+
+            services.AddDbContext<BookStoreDbContext>(options=> options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
